77e38135aff5ab07bab79762933aa915ff88ffbd
gmb28 <157037899+gmb28@users.noreply.github.com>
2024-03-01 21:01:05 -0500

    Delete backend directory

--- backend/api/ffxivData.route.js
+++
@@ -1,7 +0,0 @@
-import express from 'express'
-import ffxivDataController from './ffxivData.controller.js'
-const router = express.Router()
-
-router.route('/').get(ffxivDataController.apiGetData)
-
-export default router
